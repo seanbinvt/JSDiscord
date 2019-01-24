25f9cb7ca837da7cdcfd343ca1e5a86934cd530b
Sean Blevins <seanbinvt@users.noreply.github.com>
2019-01-24 12:46:19 -0500

    Error in commit

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 const Discord = require('discord.js');
 const client =  new Discord.Client();
-const TOKEN = 'MzMyMDI4MzEwMTEwNDcwMTQ0.DyqDaQ.rSFounTJejiuuGq2xiMaVu1BFnY';
-const Enmap = require("enmap");
-const config = require("./config.json");
+const config = require("./config.json")
 
-
-bot.on('message', function(message) {
-  if(message.content.toUpperCase() == 'FUCK <@201905314516172800>') {
-    message.reply('Fuck off');
-  }
-  if(message.content.toUpperCase() == '!roast @sean') {
-    message.channel.send(`${message.mentions.users.first().id} is a bitch`);
-  }
+client.on('ready', (message) => {
+  console.log('I am ready!');
+  const attachment = new MessageAttachment('https://78.media.tumblr.com/ea94fa4a4f00974405895b6a9fcefb32/tumblr_inline_o1l3e0IpPd1sck31p_500.gif');
+  // This event triggers when the bot joins a guild.
+  message.channel.send(attachment);
 });
 
-bot.login(TOKEN);
+  client.on("message", async message => {
+  console.log(message + ' - heard');
+    // This event will run on every single message received, from any channel or DM.
+    
+    // It's good practice to ignore other bots. This also makes your bot ignore itself
+    // and not get into a spam loop (we call that "botception").
+    if(message.author.bot) return;
+    
+    // Also good practice to ignore any message that does not start with our prefix, 
+    // which is set in the configuration file.
+    if(message.content.indexOf(config.prefix) !== 0) return;
+    //split message into array
+  const args = message.content.slice(config.prefix.length).trim().split(/ !/g);
+  const command = args[0].toLowerCase();
+  const name = args[1];
+
+  if(command=="roast") {
+    message.channel.send(`${name} is a bitch`);
+  }
+  });
+
+client.login(config.token);
