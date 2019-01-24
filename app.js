c5774199ec0652f6c918724dbc43d65aa70ff46f
Sean Blevins <seanbinvt@users.noreply.github.com>
2019-01-24 12:43:49 -0500

    Config stuff and gitignore

--- app.js
+++ app.js
@@ -14,41 +14,4 @@ bot.on('message', function(message) {
   }
 });
 
-client.commands = new Enmap();
-
-fs.readdir("./commands/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    // Load the command file itself
-    let props = require(`./commands/${file}`);
-    // Get just the command name from the file name
-    let commandName = file.split(".")[0];
-    console.log(`Attempting to load command ${commandName}`);
-    // Here we simply store the whole thing in the command Enmap. We're not running it right now.
-    client.commands.set(commandName, props);
-  });
-});
-
-module.exports = (client, message) => {
-  // Ignore all bots
-  if (message.author.bot) return;
-
-  // Ignore messages not starting with the prefix (in config.json)
-  if (message.content.indexOf(client.config.prefix) !== 0) return;
-
-  // Our standard argument/command name definition.
-  const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-
-  // Grab the command data from the client.commands Enmap
-  const cmd = client.commands.get(command);
-
-  // If that command doesn't exist, silently exit and do nothing
-  if (!cmd) return;
-
-  // Run the command
-  cmd.run(client, message, args);
-};
-//Comment
 bot.login(TOKEN);
